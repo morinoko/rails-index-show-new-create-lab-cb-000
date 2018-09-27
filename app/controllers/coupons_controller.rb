4a5ec12dc259926163b9c918c146cbf47dbd9ab3
Felice Forby <feliceforby@gmail.com>
2018-09-27 23:07:23 +0000

    set up coupon show and index pages

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,15 @@
 class CouponsController < ApplicationController
-  
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
 end
